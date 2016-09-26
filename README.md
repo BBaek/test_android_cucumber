19423f348b69be82ac968d68c8b80d4b90da4c74
WoonSung Baek <woonsungbaek@WoonSungui-MacBook-Pro.local>
2016-09-26 17:31:38 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cucumber UI Test
+Auth: Baek Woon Sung
+
+Ref: https://github.com/cucumber/cucumber-jvm.git
+
---
+++ app/src/androidTest/java/com/bbaek/cucumbertest2/test/SomeDependency.java
@@ -0,0 +1,5 @@
+package com.bbaek.cucumbertest2.test;
+
+// Dummy class to demonstrate dependency injection
+public class SomeDependency {
+}
